febc203c29f0b7251ba80d146d9bbf694b46ecee
yungshenglu <yungshenglu1994@gmail.com>
2017-10-20 11:31:28 +0800

    Submit NCTU-26151

---
+++ NCTU-26151/NCTU-26151.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    int d;
+    
+    scanf("%d", &d);
+    printf("Hello World!\n");
+
+    return 0;
+}
---
+++ NCTU-26151/NCTU-26151.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int d;
+    
+    scanf("%d", &d);
+    printf("Hello World!\n");
+
+    return 0;
+}
